bc8472cd7f7c08b07233475acc51b6afc11c3633
Ana Laura Tostes <analauratostesdasilva@gmail.com>
2022-10-30 23:22:19 -0300

    att ana

--- CQDSE/cqds/model.py
+++ CQDSE/cqds/model.py
@@ -1,6 +1,27 @@
 from tkinter import E
 from flask_login import UserMixin
 from .view import te
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+import sqlite3
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'te.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+te = SQLAlchemy(app)
+
+print(te.MetaData())
+
+#from .model import User, NewUser
+print("basedir:", basedir)
 
 
 '''''''''class User(UserMixin, te.Model):
@@ -18,5 +39,15 @@ class Sala():
     def __init__(self, nome, np):
         self.nome = nome
         self.np = np
+    def inserir_sala(self, nome, np):
+        self.nome = input("Insira o nome da sala (ex:d206):")
+        self.np = input("Insira o numero de pessoas na sala:/n")
+        conn = sqlite3.connect(os.path.join(basedir, 'te.db'))
+        c = conn.cursor()
+        c.executescript("INSERT INTO sala(nome, np) VALUES('"+nome+"','"+np+"')")
+        conn.commit()
+    
+ 
+    
 
 
